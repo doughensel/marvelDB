2478271ab04e9da3cdddf0e5e4e39079e1eed0a0
doughensel <douglas.hensel@gmail.com>
2014-03-02 16:12:37 -0500

    testing connection to marvel from remote box

--- index.php
+++ index.php
@@ -23,7 +23,28 @@
 <p>
 	<?php 
 		include_once 'php/creds.php';
-		echo $ts;
+		$url = 'http://gateway.marvel.com/v1/public/characters/?' . http_build_query($params);
+		$curl_opts = array(
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_URL => $url
+        );
+        $connection = curl_init();
+        $curl_params[CURLOPT_USERAGENT] = self::USER_AGENT;
+
+        foreach ($curl_params as $option => $value) {
+            curl_setopt($connection, $option, $value);
+        }
+        curl_setopt($connection, CURLOPT_SSL_VERIFYPEER, false);
+        $response = curl_exec($connection);
+        $headers = explode("\r\n\r\n", $response);
+        $response_code = curl_getinfo($connection, CURLINFO_HTTP_CODE);
+        $response_headers = $headers;
+        $response_json    = $response;
+        $response_obj     = json_decode($this->response_json);
+
+        curl_close($connection);
+
+        print_r( $response_obj );
 	?>
 </p>
 
